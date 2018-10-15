70081d26d71554a1e1c3fe02d12a0ad7aadb1872
JoaquinDiazRamirez <joaco_diaz_ramirez@hotmail.com>
2018-10-15 11:28:42 +0200

    Third done

--- app/src/main/java/com/example/joaco/transitiontarea/personaldata.kt
+++ app/src/main/java/com/example/joaco/transitiontarea/personaldata.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.Toast
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.CheckBox
 
 class personaldata : Activity() {
 
@@ -23,5 +24,35 @@ class personaldata : Activity() {
 
         button = findViewById(R.id.imagebutton) as Button
         button!!.setOnClickListener { Toast.makeText(this@personaldata, "Image Changed!", Toast.LENGTH_SHORT).show() }
+    }
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                R.id.checkbox_hobbie1 -> {
+                    if (checked) {
+                        // Love golf
+                    } else {
+                        // Remove the meat
+                    }
+                }
+                R.id.checkbox_hobbie2 -> {
+                    if (checked) {
+                        // Cheese me
+                    } else {
+                        // I'm lactose intolerant
+                    }
+                }
+                R.id.checkbox_hobbie3 -> {
+                    if (checked) {
+                        // Cheese me
+                    } else {
+                        // I'm lactose intolerant
+                    }
+                }
+                // TODO: Veggie sandwich
+            }
+        }
     }
 }
