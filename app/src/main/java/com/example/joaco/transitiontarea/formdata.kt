ec4f526408a04a2c6d10e09d62a09ae707cc0d4f
milw0rm55 <sebastiantace@gmail.com>
2018-10-15 09:17:36 +0200

    second screeen function

--- app/src/main/java/com/example/joaco/transitiontarea/formdata.kt
+++ app/src/main/java/com/example/joaco/transitiontarea/formdata.kt
@@ -1,5 +1,23 @@
 package com.example.joaco.transitiontarea
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class formdata : Activity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.formdata)
+
+    }
+    fun onLoginAction(view: View) {
+        Toast.makeText(this, "on login pressed", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@formdata, personaldata::class.java)
+        setContentView(R.layout.personaldata)
+        startActivity(intent)
+
+    }
+}
---
+++ app/src/main/java/com/example/joaco/transitiontarea/personaldata.kt
@@ -0,0 +1,3 @@
+package com.example.joaco.transitiontarea
+
+class personaldata
